c552c98fddddf6d3fb1ef46b78f8870c519adf19
nina-mir <ninamirf@gmail.com>
2025-08-08 21:35:03 -0700

    QuestionHeader initial design

--- src/components/question/QuestionHeader.tsx
+++ src/components/question/QuestionHeader.tsx
@@ -2,25 +2,31 @@ import { DotFilledIcon, ResetIcon } from "@radix-ui/react-icons"
 
 
 interface QuestionHeaderProps {
+    toggleFinalResult: ()=>void;
     question: string;
     id: number;
-    textColor: string; // correctClasses.text
+    textColor: string; // correctClasses.text is a tailwind utility class
 }
 
 
-const QuestionHeader: React.FC = () => {
+const QuestionHeader: React.FC<QuestionHeaderProps> = ({
+    toggleFinalResult, 
+    question,
+    id, 
+    textColor
+}) => {
     return (
         <>
             <div>
                 <span className="font-gyst">{id}</span>
-                <DotFilledIcon className={`inline w-3 h-3 ${correctClasses.text}`} />
+                <DotFilledIcon className={`inline w-3 h-3 ${textColor}`} />
                 {question}
             </div>
             <ResetIcon
                 onClick={() => toggleFinalResult()}
 
                 className={
-                    `${correctClasses.text}`
+                    `${textColor}`
                 }
             />
 
